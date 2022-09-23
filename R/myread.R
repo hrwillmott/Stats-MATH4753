146ed07a2a75d33453b531321adc232adda59a12
Heather Willmott <heatherwillmott@Heather-Ws-MacBook-Pro.local>
2022-09-23 11:39:47 -0500

    intial

--- R/myread.R
+++ R/myread.R
@@ -1,12 +1,15 @@
 #' @title myread
 #'
 #' @param csv .CSV file name
+#' @param dird directory to csv files
+#' @importFrom utils read.table
 #'
 #' @return imports a csv file into r
 #' @export
 #'
-#' @examples myread("DDT.csv")
-myread=function(csv){
+#' @examples
+#' \dontrun{myread("DDT.csv")}
+myread=function(csv,dird){
   fl=paste(dird,csv,sep="")
   read.table(fl,header=TRUE,sep=",")
 }
